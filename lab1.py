49a93bf5a883e55c052c5b3eba0add64bd0cd428
Hanzii-0425 <hansikajayamini2024@gmail.com>
2026-03-17 15:28:56 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,9 +1,14 @@
-print("Hansika")
-print("Kataragama PCC")
 
-name
+print ("Hansika")
+print ("Kataragama P.c.college")
+#this is a single line comment 
+'''This is 
+a multiline 
+comment'''
+# we use comments to understand the code clearly
+print ("22 divided by 7 is :",22/7)
+#concatenation mean joining things together usually strings
 
-# This is a multi-line comment
-# It can span multiple lines
-
-print("22 divided by 7 is:", 22 / 7) 
+operation = "Sum"
+total =8 
+print (operation + " is " + str(total))
